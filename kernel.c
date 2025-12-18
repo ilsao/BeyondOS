3798435906d2a5c90fd76c83954e0b616c3430a3
ilsao <ilsao@outlook.com>
2025-12-18 00:33:43 -0800

    feat: add putchar and printf

--- kernel.c
+++ kernel.c
@@ -1,3 +1,6 @@
+#include "kernel.h"
+#include "common.h"
+
 /* type define */
 typedef unsigned char   uint8_t;
 typedef unsigned short  uint16_t;
@@ -19,13 +22,48 @@ void *memset(void *buf, int value, size_t n)
     return buf;
 }
 
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid)
+{
+    /* define C variables and combine to correspoding register */
+    register long a0 __asm__("a0") = arg0;
+    register long a1 __asm__("a1") = arg1;
+    register long a2 __asm__("a2") = arg2;
+    register long a3 __asm__("a3") = arg3;
+    register long a4 __asm__("a4") = arg4;
+    register long a5 __asm__("a5") = arg5;
+    register long a6 __asm__("a6") = fid;
+    register long a7 __asm__("a7") = eid;
+
+    /*
+    * __asm__ __volatile__("asm" : output var : input var : clobbered regs)
+    */
+    __asm__ __volatile__(
+        "ecall"
+        : "=r"(a0), "=r"(a1)
+        : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
+        : "memory"
+    );
+
+    return (struct sbiret){.err = a0, .value = a1};
+}
+
+void putchar(char ch)
+{
+    /* eid = 1 => Console Putchar */
+    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
+}
+
 /* kernel main */
 void kernel_main(void)
 {
     // clear bss for uninitialized global and static variables
     memset((void *)__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-    for (;;);
+    printf("\n\nHello, World!\n");
+    printf("1 + 2 = %d, %x\n", 1 + 2, (1 << 31) >> 31);
+
+    for (;;)
+        __asm__ __volatile__("wfi");    // wait for interruption
 }
 
 /* booter */
